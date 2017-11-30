f6d404367392d369c67c80ae4c178f7bfb420bac
Kevin Grieger <scuffmark@gmail.com>
2017-11-30 12:30:01 -0600

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'main',
+    component: BlogListComponent
+  },
+  {
+    path: 'create',
+    component: BlogCreateComponent
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogListComponent,
+    BlogCreateComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule
   ],
   providers: [],
